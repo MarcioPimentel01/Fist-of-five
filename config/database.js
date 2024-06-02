4b5fd33a5c271cc66f304d8247200a07c826536b
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-06-01 20:37:44 -0400

    npm and npm dotenv update, environment variables working

--- config/database.js
+++ config/database.js
@@ -1,30 +1,11 @@
-// const {Sequelize} = require('sequelize')
+const {Sequelize} = require('sequelize')
 
-// require(`dotenv`).config() //? Loads environment variables from .env file. 
-// //! the .env file does not need to be in the node_modules folder, just in the same Node environment
-// //! the config() method provided by `dotenv` is used to load the environment variables
+//! the .env file does not need to be in the node_modules folder, just in the same Node environment
+//! the config() method provided by `dotenv` is used to load the environment variables
+require(`dotenv`).config() //? Loads environment variables from .env file. 
+console.log(`POSTGRESURI: ${process.env.POSTGRESURI}`)
 
-// const sequelize = new Sequelize(process.env.POSTGRESURI, {dialect: 'postgres'})
-
-// module.exports = sequelize
-
-
-const Sequelize = require('sequelize');
-
-//? Create a connection object
-const sequelize = new Sequelize(
-    //? Database name
-    `sm_app`,
-
-    //? Username
-    `mpimentel`,
-
-    `123change`,
-    {
-        //?Database location
-        host: 'localhost',
-        dialect: 'postgres',
-    }
-);
+const sequelize = new Sequelize(process.env.POSTGRESURI, {dialect: 'postgres'})
 
 module.exports = sequelize
+
