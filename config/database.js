5504a6e6214c96add3159fab668b7c3a832cd7b1
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-06-01 20:19:19 -0400

    testdatabaseconnection.js working

--- config/database.js
+++ config/database.js
@@ -1,9 +1,30 @@
-const {Sequelize} = require('sequelize')
+// const {Sequelize} = require('sequelize')
 
-require(`dotenv`).config() //? Loads environment variables from .env file. 
-//! the .env file does not need to be in the node_modules folder, just in the same Node environment
-//! the config() method provided by `dotenv` is used to load the environment variables
+// require(`dotenv`).config() //? Loads environment variables from .env file. 
+// //! the .env file does not need to be in the node_modules folder, just in the same Node environment
+// //! the config() method provided by `dotenv` is used to load the environment variables
 
-const sequelize = new Sequelize(process.env.POSTGRESURI)
+// const sequelize = new Sequelize(process.env.POSTGRESURI, {dialect: 'postgres'})
+
+// module.exports = sequelize
+
+
+const Sequelize = require('sequelize');
+
+//? Create a connection object
+const sequelize = new Sequelize(
+    //? Database name
+    `sm_app`,
+
+    //? Username
+    `mpimentel`,
+
+    `123change`,
+    {
+        //?Database location
+        host: 'localhost',
+        dialect: 'postgres',
+    }
+);
 
 module.exports = sequelize
