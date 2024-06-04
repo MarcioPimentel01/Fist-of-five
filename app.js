f953cc9ec4680c2c86349bb25e41d3f7a41b86a5
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-06-04 13:41:38 -0400

    Revert "handlebars.js"

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-const express = require('express');
-const exphbs  = require('express-handlebars');
-const app = express();
+const express = require(`express`)
 
-// Setup handlebars engine
-app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main"}));
-app.set('view engine', 'handlebars');
+const app = express()
 
-// Route for rendering a view
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-// Route for REST API endpoint
-app.get('/api/status', (req, res) => {
-    res.status(200).json({ status: 'ok', message: 'REST API is up and running' });
-});
+app.get (`/`, (req, res) => {
+    res.status(200).json(
+        { status: `ok`, message: `REST API is up and running` }
+    )
+})
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+    console.log(`Server is running on port 3000`)
+})
