f1aa0faf7a232c1b53ee7dfea5c479b4bb793dc0
beyondthefold777 <beyondthefold1989@gmail.com>
2024-06-06 16:02:50 -0400

    setting up login controller with jwt function with thorough functuality commenting and testing to see if env file gets ignored before adding sensitive info

---
+++ public/routes/loginroutes.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const getUser = async (username) => {
+    return { userId: 123, password: "123456", username };
+};
+
+module.exports = asunc (req, res) => {
+const { username, password } = req.body;
+
+const user = await getUser(username);
+
+if (user.password !== password) {
+    return res.status(403).json({
+        error: "invalid login",
+    })
+}
+
+delete user.password;
+
+const token = jwt.sign(user, process.env.MY_SECRET, {expiresIn: "1h" })
+
+res.cookie("token", token, {
+    httpOnly: true,
+}
+return res.redirect("/welcome");
